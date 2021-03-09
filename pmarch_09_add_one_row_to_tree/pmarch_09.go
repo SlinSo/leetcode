ae435ae7659b42209d2406ee9e377f64b4cccc7e
slinso <sven@lindow-software.de>
2021-03-09 14:09:50 +0100

    march 09 and p623

---
+++ pmarch_09_add_one_row_to_tree/pmarch_09.go
@@ -0,0 +1,42 @@
+package pmarch_09
+
+import (
+	"github.com/SlinSo/leetcode/structure"
+)
+
+type TreeNode = structure.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func addOneRow(root *TreeNode, v int, d int) *TreeNode {
+	return addNode(root, v, d, true)
+}
+
+func addNode(node *TreeNode, v int, d int, left bool) *TreeNode {
+	if d == 1 {
+		t := &TreeNode{Val: v}
+		if left {
+			t.Left = node
+		} else {
+			t.Right = node
+		}
+		return t
+	} else if d == 2 {
+		node.Left = addNode(node.Left, v, d-1, true)
+		node.Right = addNode(node.Right, v, d-1, false)
+	} else {
+		if node.Left != nil {
+			node.Left = addNode(node.Left, v, d-1, true)
+		}
+		if node.Right != nil {
+			node.Right = addNode(node.Right, v, d-1, false)
+		}
+	}
+	return node
+}
---
+++ pmarch_09_add_one_row_to_tree/pmarch_09_test.go
@@ -0,0 +1,95 @@
+package pmarch_09
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/SlinSo/leetcode/structure"
+)
+
+func Test_addOneRow(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		v    int
+		d    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "ex1",
+			args: args{
+				root: &structure.TreeNode{Val: 4, Left: &structure.TreeNode{Val: 2}, Right: &structure.TreeNode{Val: 6}},
+				v:    1,
+				d:    2,
+			},
+			want: &structure.TreeNode{Val: 4,
+				Left: &structure.TreeNode{Val: 1,
+					Left: &structure.TreeNode{Val: 2},
+				},
+				Right: &structure.TreeNode{Val: 1,
+					Right: &structure.TreeNode{Val: 6}}},
+		},
+		{
+			name: "ex2",
+			args: args{
+				root: &structure.TreeNode{Val: 4,
+					Left:  &structure.TreeNode{Val: 2},
+					Right: &structure.TreeNode{Val: 6}},
+				v: 1,
+				d: 3,
+			},
+			want: &structure.TreeNode{Val: 4,
+				Left: &structure.TreeNode{Val: 2,
+					Left:  &structure.TreeNode{Val: 1},
+					Right: &structure.TreeNode{Val: 1},
+				},
+				Right: &structure.TreeNode{Val: 6,
+					Left:  &structure.TreeNode{Val: 1},
+					Right: &structure.TreeNode{Val: 1},
+				},
+			},
+		},
+		{
+			name: "ex3",
+			args: args{
+				root: &structure.TreeNode{Val: 4,
+					Left: &structure.TreeNode{Val: 2,
+						Left:  &structure.TreeNode{Val: 3},
+						Right: &structure.TreeNode{Val: 1},
+					},
+					Right: &structure.TreeNode{Val: 6,
+						Left: &structure.TreeNode{Val: 5},
+					},
+				},
+				v: 1,
+				d: 3,
+			},
+			want: &structure.TreeNode{Val: 4,
+				Left: &structure.TreeNode{Val: 2,
+					Left: &structure.TreeNode{Val: 1,
+						Left: &structure.TreeNode{Val: 3},
+					},
+					Right: &structure.TreeNode{Val: 1,
+						Right: &structure.TreeNode{Val: 1},
+					},
+				},
+				Right: &structure.TreeNode{Val: 6,
+					Left: &structure.TreeNode{Val: 1,
+						Left: &structure.TreeNode{Val: 5},
+					},
+					Right: &structure.TreeNode{Val: 1},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addOneRow(tt.args.root, tt.args.v, tt.args.d); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addOneRow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
